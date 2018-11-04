5daf095a6c59054f39c2dfaf993e6d8af7d08bb0
tobyl <tobyleftly@gmail.com>
2018-11-04 13:33:09 -0500

    Updates

---
+++ precache-manifest.106244f3ab4ce032c0285d45abf4905a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f65904306da42969d6a84048df3261a",
+    "url": "/leftly-me/static/media/old-toby-headshot-avatar.3f659043.png"
+  },
+  {
+    "revision": "508af8688c346cd08fe7d8a7425a7aa3",
+    "url": "/leftly-me/static/media/bg.508af868.jpg"
+  },
+  {
+    "revision": "602514b13db2e3b4be57",
+    "url": "/leftly-me/static/js/runtime~main.602514b1.js"
+  },
+  {
+    "revision": "841553337876efb353e9",
+    "url": "/leftly-me/static/js/main.84155333.chunk.js"
+  },
+  {
+    "revision": "80f5e66034dd70ca12db",
+    "url": "/leftly-me/static/js/1.80f5e660.chunk.js"
+  },
+  {
+    "revision": "841553337876efb353e9",
+    "url": "/leftly-me/static/css/main.19e19734.chunk.css"
+  },
+  {
+    "revision": "fe83b5644f886ea1e44783878620c456",
+    "url": "/leftly-me/index.html"
+  }
+];
