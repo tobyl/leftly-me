7cabdf8eeac751be83f9e3d7f7634971d4c5b70d
tobyl <tobyleftly@gmail.com>
2018-12-03 13:15:18 -0500

    Added cname, fix for api call.

--- src/PostModal/index.js
+++ src/PostModal/index.js
@@ -12,13 +12,23 @@ class PostModal extends React.Component {
     return distanceInWords(this.props.post.created, new Date())
   }
   render() {
-    const { body, avatar } = this.props
+    const { body, avatar, post } = this.props
+    console.log(post)
     return (
       <div className="PostModal">
         <div className="ModalBody">
-          {avatar}
-          <div className="PostMeta">{this.getTime()} ago</div>
-          {body}
+          <div className="PostMeta clearfix">
+            {avatar}
+            <p>
+              {post.tweet_owner ?
+                <span><a href={`https://twitter.com/${post.tweet_owner}`}>@{post.tweet_owner}</a> tweeted<br /></span> :
+                <span>Toby posted<br /></span>}
+              {this.getTime()} ago
+            </p>
+          </div>
+          <div className="PostBody">
+            {body}
+          </div>
           <Link to="/" className="Close">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 212.98 212.98"><path d="M131.8 106.5l75.94-75.95a17.9 17.9 0 1 0-25.31-25.3l-75.94 75.93L30.55 5.24a17.9 17.9 0 1 0-25.3 25.31l75.93 75.94-75.94 75.94a17.9 17.9 0 0 0 25.31 25.3l75.94-75.93 75.94 75.94a17.9 17.9 0 1 0 25.31-25.31l-75.94-75.94z" fillRule="evenodd" clipRule="evenodd" id="Close" /></svg>
           </Link>
